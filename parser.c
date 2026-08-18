981445759776a10f1d297c494aa4dbdf9b8f8377
Jennifer Perez <strawberries2254@gmail.com>
2026-08-17 20:40:04 -0400

    working on phase 2, got the callback function written, but needs to be tested, and I need to call the phase 3 functions

--- parser.c
+++ parser.c
@@ -4,9 +4,36 @@
 #include <stdlib.h>
 
 void packet_handler(u_char *user, const struct pcap_pkthdr *h,
-    const u_char *bytes)
+    const u_char *packet)
 {
     (void)user;
     (void)h;
-    (void)bytes;
+    // Delcaring containers
+    const struct sniff_ethernet *ethernet;
+    const struct sniff_ip *ip;
+    const struct sniff_tcp *tcp;
+    const u_char *payload;
+
+    u_int size_ip;
+    u_int size_tcp;
+
+    // Typecasting
+    ethernet = (struct sniff_ethernet*)(packet);
+    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20) {
+        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20) {
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+    payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+    (void)payload;
+    (void)ethernet;
+    (void)ip;
+    (void)tcp;
 }
