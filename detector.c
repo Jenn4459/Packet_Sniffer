796c9c04cf4261555ee5b39c7a1ca4cec2e594de
Jennifer Perez <strawberries2254@gmail.com>
2026-08-26 17:52:45 -0400

    working on detector.c - stuck on check_rates

--- detector.c
+++ detector.c
@@ -5,13 +5,87 @@
 #include <stdlib.h>
 
 // i will need to return something, but not sure what yet
-void threat_checks(const struct sniff_ip *ip, const struct sniff_tcp *tcp, 
+char* threat_checks(const struct sniff_ip *ip, const struct sniff_tcp *tcp, 
         const u_char *payload, u_int payload_len, 
         const struct sniff_ethernet *ethernet)
 {
-    (void)ip;
-    (void)tcp;
-    (void)payload;
-    (void)payload_len;
-    (void)ethernet;
+    (void)ip; (void)ethernet;
+
+    int flags = check_flags(tcp);
+    int sig = check_signatures(payload, payload_len);
+    bool rates = check_rates(tcp);
+}
+
+int check_flags(const struct sniff_tcp *tcp)
+{
+    if (tcp->th_flags & TH_FIN & TH_PUSH & TH_URG) {
+        return 1;
+    }
+    if (tcp->th_flags == 0) {
+        return 2;
+    }
+    if (tcp->th_flags & TH_SYN & TH_FIN) {
+        return 3;
+    }
+    return 0;
+}
+
+int check_signatures(const u_char *payload, u_int payload_len)
+{
+    if (payload_len > 0 && payload != NULL) {
+        // check SQL
+        const char *SQL = "' OR '1'='1";
+        if (memmem(payload, payload_len, SQL, strlen(SQL)) != NULL) {
+            return 1;
+        }
+        // check XSS
+        const char *XSS_1 = "<script>alert(1)</script>";
+        const char *XSS_2 = "<img src=x onerror=alert(1)>";
+        const char *XSS_3 = "<img src=x onerror=alert1>";
+        if (memmem(payload, payload_len, XSS_1, strlen(XSS_1)) != NULL ||
+            memmem(payload, payload_len, XSS_2, strlen(XSS_2)) != NULL ||
+            memmem(payload, payload_len, XSS_3, strlen(XSS_3)) != NULL) {
+            return 2;
+        }
+        // check RCE
+        const char *RCE_1 = "/bin/bash";
+        const char *RCE_2 = "$(cat /etc/passwd)";
+        const char *RCE_3 = "`";
+        const char *RCE_4 = "; ";
+        const u_char buff[] = {0x90, 0x90, 0x90, 0x90};
+        if (memmem(payload, payload_len, RCE_1, strlen(RCE_1)) != NULL ||
+            memmem(payload, payload_len, RCE_2, strlen(RCE_2)) != NULL ||
+            memmem(payload, payload_len, RCE_3, strlen(RCE_3)) != NULL ||
+            memmem(payload, payload_len, RCE_4, strlen(RCE_4)) != NULL ||
+            memmem(payload, payload_len, buff, sizeof(buff)) != NULL) {
+            return 3;
+        }
+    }
+    return 0;
+}
+
+bool check_rates(const struct sniff_tcp *tcp)
+{
+    // not using time but a rate in which rst flags are 
+    // present in a window of 60 packets
+    // using a circular buffer to adjust the window, but 
+    // having issues with the initial setup of the array
+    const float rate = 1.0f / 10.0f;
+    bool buffer[60];
+    bool count = 0;
+    static int first = 0;
+    static int last = 0;
+
+    // if (last ) {
+
+    // }
+    if (tcp->th_flags & TH_RST) {
+        buffer[last] = true;
+    } else {
+        buffer[last] = false;
+    }
+
+    last = (last + 1) % 60;
+
+    return false;
 }
--- detector.h
+++ detector.h
@@ -1,9 +1,13 @@
 #ifndef DETECTOR_H
 #define DETECTOR_H
 
+#define _GNU_SOURCE
+
     #include <pcap/pcap.h>
     #include <sys/types.h>
     #include <netinet/in.h>
+    #include <stdbool.h>
+    #include <string.h>
 
     typedef unsigned char  u_char;
     typedef unsigned short u_short;
@@ -13,8 +17,12 @@
     struct sniff_ip;
     struct sniff_tcp;
 
-    void threat_checks(const struct sniff_ip *ip, const struct sniff_tcp *tcp, 
+    char* threat_checks(const struct sniff_ip *ip, const struct sniff_tcp *tcp, 
         const u_char *payload, u_int payload_len, 
         const struct sniff_ethernet *ethernet);
+
+    int check_flags(const struct sniff_tcp *tcp);
+    int check_signatures(const u_char *payload, u_int payload_len);
+    bool check_rates(const struct sniff_tcp *tcp);
 
 #endif
